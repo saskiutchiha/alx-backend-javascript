82a8028d0f64cc10a0d66d323d075cda6b56c234
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2024-04-23 00:03:59 +0100

    Create 3-default-parameter.js

---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,10 @@
+export default function getSumOfHoods(initialNumber, expansion1989 = default, expansion2019 = default) {
+  if (expansion1989 === undefined) {
+    expansion1989 = 89;
+  }
+
+  if (expansion2019 === undefined) {
+    expansion2019 = 19;
+  }
+  return initialNumber + expansion1989 + expansion2019;
+}
