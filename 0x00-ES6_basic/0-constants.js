563fd5aefc8e15e0c4370779bea9e7ac32f67cf1
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2024-04-22 23:34:40 +0100

    Update 0-constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,5 +1,5 @@
 export function taskFirst() {
-  let task = 'I prefer const when I can.';
+  const task = 'I prefer const when I can.';
   return task;
 }
 
@@ -8,7 +8,7 @@ export function getLast() {
 }
 
 export function taskNext() {
-  let combination = 'But sometimes let';
+  const combination = 'But sometimes let';
   combination += getLast();
 
   return combination;
